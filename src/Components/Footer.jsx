e3ba9b445ebf7f2dfe623472cd5de5a04e1fbf17
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-11-08 17:56:34 +0530

    pre- chatgpt code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import gmail from '../assets/gmail.png'
+import insta from '../assets/insta.png'
+import whatsapp from '../assets/whatsapp.png'
+import linkedin from '../assets/linkedin.png'
 
 const Footer = () => {
+
+  const ContactItems = [
+    {
+      id:1,
+      ImagePath:gmail,
+      to:'mailto:brainstreamliner@gmail.com',
+    },{
+      id:2,
+      ImagePath:whatsapp,
+      to:'https://api.whatsapp.com/send?phone=918708992663',
+    },{
+      id:3,
+      ImagePath:insta,
+      to:'https://www.instagram.com/brainstreamliner/',
+    },{
+      id:4,
+      ImagePath:linkedin,
+      to:'https://www.linkedin.com/in/gayatri-ahuja7988417778/'
+    }
+  ]
+
+
   return (
-    <div className="sm:flex  h-60 sm:h-44 w-screen bg-[#E5E4E2] text-black">
+    <div className="sm:flex  h-60 sm:h-28 w-screen bg-[#E5E4E2] text-black">
       <div className="sm:w-1/2 flex flex-col items-start sm:ml-20 justify-center sm:border-r-2 ">
         <div className="flex">
           <p className="font-bold mr-7">Address: </p>
@@ -16,6 +42,14 @@ const Footer = () => {
             </div>
           
         </div>
+        <div className='gap-2 h-full flex'>
+            {ContactItems.map(item=>(
+              
+              <a key={item.id} href={item.to} className=''>
+                <img src={item.ImagePath} className='w-6   h-6' alt="" />
+              </a>
+            ))}
+          </div>
       </div>
 
       <div className='flex items-center justify-center sm:w-1/2'>
